ef34c247ab2813d44555b6896709c341e813557a
Jarek Rozanski <jrozanski@inputobjects.eu>
2023-05-18 12:13:43 +0000

    Explicitly export initialization function

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import { initWideAngle } from "./dist";
 import { ref } from 'vue';
 
+export { initWideAngle } from "./dist";
 export default {
-  install: async (app, options) => {
+  install: (app, options) => {
     const waaRef = ref()
     app.provide('waa', waaRef);
     initWideAngle(options)
       .then(waa => {
         waaRef.value = waa;
         console.debug("[WAA] Wide Angle Analytics instance available");
-      })
-      .catch(e => { console.error("Failed to load Wide Angle Plugin", e)});
+      }).catch(e => { console.error("[WAA] Failed to load Wide Angle Plugin", e)});
+    return waaRef;
   }
 }
